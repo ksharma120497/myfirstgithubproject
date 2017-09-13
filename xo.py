c1100c4aa486709d7685aaa6aa84e02580ae0fd8
ksharma120497 <ksharma120497@gmail.com>
2017-09-13 20:50:59 +0530

    Scoring Scheme

--- xo.py
+++ xo.py
@@ -1,124 +1,160 @@
 import tkinter
+from tkinter import messagebox
 top=tkinter.Tk()
 i=1
-j=0
-answers=[]
+count=0
+
 def score():
-        if 
-def value(n):
-        global j
-        global answers
-        answers.append(n)
+        if(btn10['text']=="             " or btn11['text']=="             " or btn12['text']=="             " or btn20['text']=="             " or btn21['text']=="             " or btn22['text']=="             " or btn30['text']=="             " or btn31['text']=="             " or btn32['text']=="             "):
+                return 0 
+        elif(btn10['text']==btn20['text'] and btn20['text']==btn30['text']):
+                winner=btn10['text']
+                messagebox.showinfo("Winner",winner+" wins! ")
+        elif(btn11['text']==btn21['text'] and btn21['text']==btn31['text']):
+                winner=btn11['text']
+                messagebox.showinfo("Winner",winner+" wins!")
+        elif(btn12['text']==btn22['text'] and btn22['text']==btn32['text']):
+                winner=btn12['text']
+                messagebox.showinfo("Winner",winner+" wins!")
+        elif(btn10['text']==btn11['text'] and btn11['text']==btn12['text']):
+                winner=btn10['text']
+                messagebox.showinfo("Winner",winner+" wins!")
+        elif(btn20['text']==btn21['text'] and btn21['text']==btn22['text']):
+                winner=btn20['text']
+                messagebox.showinfo("Winner",winner+" wins! ")
+        elif(btn30['text']==btn31['text'] and btn31['text']==btn32['text']):
+                winner=btn30['text']
+                messagebox.showinfo("Winner",winner+" wins! ")
+        
+
 
 def chng10():
-        global i
+        global i,count
         if i==1:
             btn10.config(text='X')
             btn10.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn10.config(text='O')
             btn10.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
             
 def chng11():
-        global i
+        global i,count
         if i==1:
             btn11.config(text='X')
             btn11.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn11.config(text='O')
             btn11.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def chng12():
-        global i
+        global i,count
         if i==1:
             btn12.config(text='X')
             btn12.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn12.config(text='O')
             btn12.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def chng20():
-        global i
+        global i,count
         if i==1:
             btn20.config(text='X')
             btn20.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn20.config(text='O')
             btn20.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def chng21():
-        global i
+        global i,count
         if i==1:
             btn21.config(text='X')
             btn21.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn21.config(text='O')
             btn21.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def chng22():
-        global i
+        global i,count
         if i==1:
             btn22.config(text='X')
             btn22.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn22.config(text='O')
             btn22.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def chng30():
-        global i
+        global i,count
         if i==1:
             btn30.config(text='X')
             btn30.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn30.config(text='O')
             btn30.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def chng31():
-        global i
+        global i,count
         if i==1:
             btn31.config(text='X')
             btn31.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn31.config(text='O')
             btn31.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def chng32():
-        global i
+        global i,count
         if i==1:
             btn32.config(text='X')
             btn32.config(state="disabled")
             i=0;
-            value('X')
+            count=count+1
+            score()
         else:
             btn32.config(text='O')
             btn32.config(state="disabled")
             i=1;
-            value('O')
+            count=count+1
+            score()
 def newgame():
     btn10.config(text="             ")
     btn11.config(text="             ")
@@ -129,7 +165,15 @@ def newgame():
     btn30.config(text="             ")
     btn31.config(text="             ")
     btn32.config(text="             ")
-    
+    btn10.config(state="normal")
+    btn11.config(state="normal")
+    btn12.config(state="normal")
+    btn20.config(state="normal")
+    btn21.config(state="normal")
+    btn22.config(state="normal")
+    btn30.config(state="normal")
+    btn31.config(state="normal")
+    btn32.config(state="normal")
     
 btn10=tkinter.Button(top,text="             ",command=chng10)
 btn10.grid(row=1,column=0)
@@ -149,7 +193,6 @@ btn31=tkinter.Button(top,text="             ",command=chng31)
 btn31.grid(row=3,column=1)
 btn32=tkinter.Button(top,text="             ",command=chng32)
 btn32.grid(row=3,column=2)
-
 btn1=tkinter.Button(top,text="New Game",command=newgame)
 btn1.grid(row=6,column=1)
 top.title("X & O")
